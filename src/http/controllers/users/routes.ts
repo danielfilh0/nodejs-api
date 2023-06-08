f6243586bbdae80b8244f002eb304a3c52ba9c02
Daniel Filho <danbsilva98@gmail.com>
2023-06-08 16:00:28 -0300

    feat: add authentication controller with JWT

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,8 +2,10 @@ import { FastifyInstance } from 'fastify'
 
 import { listUsers } from './list-users'
 import { createUser } from '@/http/controllers/users/create-user'
+import { authenticate } from './authenticate'
 
 export async function userRoutes(app: FastifyInstance) {
   app.get('/users', listUsers)
   app.post('/users', createUser)
+  app.post('/sessions', authenticate)
 }

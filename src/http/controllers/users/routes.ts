c6eea4e47f77637d79b9e933818d2a71b1ba572c
Daniel Filho <danbsilva98@gmail.com>
2023-06-29 12:11:50 -0300

    fix: fix users routes

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -8,12 +8,13 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { deleteUser } from './delete-user'
 import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 import { updateUserRole } from './update-user-role'
+import { updateUser } from './update-user'
 
 export async function userRoutes(app: FastifyInstance) {
-  app.get('/users', listUsers)
+  app.get('/users',{ onRequest: [verifyUserRole('ADMIN')] }, listUsers, )
   app.post('/users', createUser)
   app.post('/sessions',  authenticate)
-  app.put('/users', createUser)
+  app.put('/users', { onRequest: [verifyJWT] }, updateUser)
   app.patch('/users/photo', { onRequest: [verifyJWT] }, updateUserPhoto)
   app.patch('/users/:userId/role', { onRequest: [verifyJWT, verifyUserRole('ADMIN')] }, updateUserRole)
   app.delete('/users/:userId', { onRequest: [verifyJWT, verifyUserRole('ADMIN')] }, deleteUser)
